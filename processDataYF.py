889a213367e73fca9da05deccec021ea5e80512b
gabrielsabinocastro <gabrielsabinocastro@usp.br>
2022-08-14 17:42:44 -0300

    desenvolvendo e documentando classe e funções

--- processDataYF.py
+++ processDataYF.py
@@ -9,8 +9,9 @@ class DataYFinance():
     """
     def __init__(self, tickers):
         """
-        Parâmetros:
-            tickers (array de strings): ativos 
+        Parâmetro:
+            tickers (array de strings): códigos para ativos bando de dados YahooFinance.
+
         """
         super().__init__()
         self.tickers = tickers
@@ -19,38 +20,79 @@ class DataYFinance():
         """
         Função que recebe uma lista de tickers e retorna um dataframe com os preços 
         ajustados de cada um dos tickers, com base nos dados extraídos do YahooFinance.
-        startDate: data mais antiga
-        endDate: data mais recente
+        Recebe:
+            startDate (objeto datetime.date ou string 'YYYY-MM-DD'): data mais antiga para a série.
+            endDate (objeto datetime.date ou string 'YYYY-MM-DD'): data mais recente para a série.
+        Devolve:
+            Pandas.DataFrame: dataframe com os preços diários ajustados.        
         """
+        
         df = yf.download(self.tickers, startDate, endDate)
         df = df["Adj Close"]
+        
         return df
 
-    def mReturns(self, adjclose):
+    def mReturns(self, adjclose): #TODO adapatar para DataFrame, funciona epanas com DataSeries
         """
-        para dataframe com retornos mensais.
+        Recebe:
+            adjclose (Pandas.DataFrame / .Series): preços diários ajustados.
+        Devolve:
+            Pandas.DataFrame / .Series: dataframe/series com os retornos mensais.        
         """
+        
+        adjclose = adjclose.rename('Returns')
         temp = (adjclose.pct_change().dropna()).resample("M").apply(lambda x: ((1+x).prod()) - 1)
         temp.index = pd.to_datetime(temp.index, format="%Y%m").to_period('M')
+        
         return temp
 
-    def dReturns(self, adjclose):
+    def dReturns(self, adjclose): #TODO adapatar para DataFrame, funciona epanas com DataSeries
         """
-        para retornos diários.
+        Recebe:
+            adjclose (Pandas.DataFrame / .Series): preços diários ajustados.
+        Devolve:
+            Pandas.DataFrame / .Series: dataframe/series com os retornos diários.
         """
+        
+        adjclose = adjclose.rename('Returns')
         temp = adjclose.dropna().pct_change()
+        
         return temp.dropna()
     
-    def mVol(self, adjclose):
+    def mVol(self, adjclose): #TODO adapatar para DataFrame, funciona epanas com DataSeries
+        """
+        Recebe:
+            adjclose (Pandas.DataFrame / .Series): preços diários ajustados.
+        Devolve:
+            numpy.float64: volatilidade (simples) mensal.
+        """
+        
         temp = self.mReturns(adjclose).std()
+        
         return temp
 
-    def dVol(self, adjclose):
+    def dVol(self, adjclose): #TODO adapatar para DataFrame, funciona epanas com DataSeries
+        """
+        Recebe:
+            adjclose (Pandas.DataFrame / .Series): preços diários ajustados.
+        Devolve:
+            numpy.float64: volatilidade (simples) diária.
+        """
+        
         temp = self.dReturns(adjclose).std()
+        
         return temp
 
-    def aVol(self, adjclose):
+    def aVol(self, adjclose): #TODO adapatar para DataFrame, funciona epanas com DataSeries
+        """
+        Recebe:
+            adjclose (Pandas.DataFrame / .Series): preços diários ajustados.
+        Devolve:
+            numpy.float64: volatilidade (simples) anualizada.
+        """
+        
         temp = (252 ** (1/2)) * self.dVol(adjclose)
+        
         return temp
     
     
--- quantiTools.py
+++ quantiTools.py
@@ -1,10 +1,58 @@
 import pandas as pd
 import numpy as np
+import datetime as dt
+from dateutil.relativedelta import relativedelta as rltd
 
 
-def movVol(dReturns, window):
+def rolVol(dReturns, window): #TODO Volatility Rolling
     """
     Recebe um pdDataFrame com os retornos diários e um número natural de intervalo e retorna.
     """
     temp = dReturns.rolling(window=window).std()
     return temp
+
+def clusterVol(dReturns, window, weight): #TODO Volatility Cluster
+    """
+    Calcula uma lista de cluster de volatlilidade.
+    """
+    pass
+
+
+def distrReturns(dReturns): #TODO Distribuition of Returns
+    """
+    Calcula uma lista com a distribuição dos retornos.
+    """
+    pass
+
+def maxDDown(dReturns): #TODO maximum drawdown
+
+    pass
+
+def benchmarkBeta(dReturns, benchmarkReturns): #TODO Linear Beta
+    #devolver uma tupla com o angular da regressão linear e o r^2
+    #observar a quantidade de dias, anual, mensal...
+
+    pass
+
+def ewmaVol(dReturns, window, lamb = 0.94): #TODO ajustar código para que funcione com DataFrame, está trabalhando apenas com DataSeries
+    """
+    Recebe:
+        dReturns (Pandas.DataFrame / .Series): retornos diários.
+        window (int): número no intervalo da série de retornos, para o range da volatilidade.
+        lam (float): parâmetro do decaimento exponencial
+    Devolve:
+        numpy.float64: volatilidade diária EWMA.
+    """
+
+    temp = pd.DataFrame(dReturns)
+    temp = temp[len(dReturns)- window:]
+    temp['Expo'] = np.arange(window-1,-1,-1)
+    temp['EWMA Vol'] = temp.apply(lambda x: (x['Returns']**2)* (1 - lamb) * (lamb) **(x['Expo']) , axis=1)
+
+    return np.sqrt(temp['EWMA Vol'].sum())
+
+def rolewmaVol(dReturns, window, lamb = 0.94):
+    # (sigma^2)_t =  (1 - lambda) * (r^2)_t-1  +  lambda * (sigma^2)_t-1
+    temp = 0
+
+    pass
